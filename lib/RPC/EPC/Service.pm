ada6e16dc80a11bc4f3a3493bf7d6d8181d96cd8
Masashi Sakurai <m.sakurai@kiwanami.net>
2011-12-26 05:22:31 +0000

    Import of KIWANAMI/RPC-EPC-Service-v0.0.2 from CPAN.
    gitpan-cpan-distribution: RPC-EPC-Service
    gitpan-cpan-version:      v0.0.2
    gitpan-cpan-path:         KIWANAMI/RPC-EPC-Service-v0.0.2.tar.gz
    gitpan-cpan-author:       KIWANAMI
    gitpan-cpan-maturity:     released

--- lib/RPC/EPC/Service.pm
+++ lib/RPC/EPC/Service.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use Carp;
 
-use version; our $VERSION = qv('0.0.1');
+use version; our $VERSION = qv('0.0.2');
 
 use base 'Exporter';
 
@@ -348,7 +348,7 @@ RPC::EPC::Service - An Asynchronous Remote Procedure Stack.
 
 =head1 VERSION
 
-This document describes RPC::EPC::Service version 0.0.1
+This document describes RPC::EPC::Service version 0.0.2
 
 
 =head1 SYNOPSIS
@@ -507,7 +507,7 @@ If connection failed, it will die.
 
 =head3 stop
 
-  $service->client_start;
+  $service->stop;
 
 Shutdown the connection.
 
